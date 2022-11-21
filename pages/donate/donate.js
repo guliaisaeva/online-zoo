575de3294323e2ecb1d678e1c884f957db265cda
Gulia <gulia.isaeva.pls@gmail.com>
2022-11-21 08:04:39 +0300

    feat: add donate icons,style,html markup

---
+++ pages/donate/donate.js
@@ -0,0 +1,23 @@
+const radio=document.querySelectorAll("input[type=radio]");
+console.log(radio)
+const label=document.querySelectorAll("label");
+console.log(label)
+
+
+// for (let index = 0; index < radio.length; index++) {
+//     const element = radio[index];
+//     element.addEventListener('change', function() {
+//         if (this.checked) {
+//           console.log("radio is checked..");
+//           for (let index = 0; index < label.length; index++) {
+//             const lbl = label[index];
+//             lbl.style.color="green"
+//           }
+//         } else {
+//           console.log("radio is not checked..");
+//         }
+//       });
+// }
+
+
+
