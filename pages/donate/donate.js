b5485d1bc1df2b562691b3970a85fa5ac6af93ab
Gulia <gulia.isaeva.pls@gmail.com>
2022-12-19 22:54:05 +0300

    feat:add js functionality

--- pages/donate/donate.js
+++ pages/donate/donate.js
@@ -23,11 +23,48 @@ console.log(label)
 const burgerMenu=document.querySelector("#burger-menu");
 const navEl=document.querySelector(".nav__menu");
 const designerEl=document.querySelector(".nav__designer");
+const mainEl=document.querySelector("main");
+const closeBtn=document.querySelector(".close-icon");
+const footerEl=document.querySelector("footer");
+
 burgerMenu.addEventListener("click",()=>{
-navEl.classList.toggle("active");
-designerEl.classList.toggle("active");
+openClose();
+mainEl.addEventListener("click",()=>{
+    navEl.classList.remove("active");
+    designerEl.classList.remove("active");
+    mainEl.classList.remove("active");
+    closeBtn.classList.remove("active")
+    footerEl.classList.remove("active");
 })
+})
+closeBtn.addEventListener("click",openClose)
 
+function openClose(){
+    navEl.classList.toggle("active");
+    designerEl.classList.toggle("active");
+    mainEl.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    footerEl.classList.toggle("active");
+}
 
+// Amount functionality
+const barEl=document.getElementsByClassName("amount");
+const amountEl=document.querySelector(".number-input")
+Array.from(barEl).forEach(element => {
+    element.addEventListener("change",(e)=>{
+       let value=e.target.value;
+  amountEl.value=amountEl.textContent=value;
+    })
+});
 
+amountEl.addEventListener("input",(e)=>{
+    Array.from(barEl).forEach(element => {
+        element.addEventListener("change",(e)=>{
+           let value=e.target.value;
+      amountEl.value=amountEl.textContent=value;
 
+    if(amountEl.value===value){
+       console.log("super")
+    }
+})})
+  })
--- pages/main/script.js
+++ pages/main/script.js
@@ -43,7 +43,132 @@ btn.addEventListener("click",function(){
 const burgerMenu=document.querySelector("#burger-menu");
 const navEl=document.querySelector(".nav__menu");
 const designerEl=document.querySelector(".nav__designer");
+const mainEl=document.querySelector("main");
+const closeBtn=document.querySelector(".close-icon");
+const footerEl=document.querySelector("footer");
+
 burgerMenu.addEventListener("click",()=>{
-navEl.classList.toggle("active");
-designerEl.classList.toggle("active");
+    openClose();
+    mainEl.addEventListener("click",()=>{
+        navEl.classList.remove("active");
+        designerEl.classList.remove("active");
+        mainEl.classList.remove("active");
+        closeBtn.classList.remove("active")
+        footerEl.classList.remove("active")
+    })
+    })
+    closeBtn.addEventListener("click",openClose)
+
+    function openClose(){
+        navEl.classList.toggle("active");
+        designerEl.classList.toggle("active");
+        mainEl.classList.toggle("active");
+        closeBtn.classList.toggle("active");
+        footerEl.classList.toggle("active")
+    }
+
+    //Carousel in a block Pets
+
+    // let sliderMain=document.querySelector(".animal-cards__slider");
+    // console.log(sliderMain)
+    // let item=sliderMain.getElementsByClassName("animal-cards__content");
+    // console.log(item)
+    // function next(){
+    //   console.log("works")
+    //  sliderMain.appendChild(item[0])
+    // }
+    // next();
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("animal-cards__card");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "block";
+  }
+
+  slides[slideIndex-1].style.display = "none";
+
+}
+
+// Popup testimonials
+const testimonialsCards=document.querySelector(".testimonials__cards");
+const card=document.querySelectorAll(".testimonials__card");
+Array.from(card).forEach(element=>{
+  element.addEventListener("click",()=>{
+    createPopup();
+
+  })
 })
+
+
+
+// testimonialsCards.addEventListener("click",createPopup)
+function createPopup(){
+  //  const popup=document.createElement("div");
+  //  popup.setAttribute("class",
+  //  "popup");
+  //  const popupCard= document.createElement("div");
+  //  popupCard.classList.add("popup-card","testimonials__card")
+  //  popup.appendChild(popupCard);
+  const modalEl=document.getElementsByClassName("modal");
+   Array.from(modalEl).forEach(element=>{
+    element.innerHTML=`
+    <div class="testimonials__card popup">
+    <figure class="testimonials__header">
+      <img src="../../assets/icons/testimon-footer/user_icon.svg" alt="user icon">
+        <figcaption>
+         <p class="user_name">Michael John</p>
+         <div class="user_info">
+             <p class="user_data">Local Austria</p>
+             <p class="user_data"> &middot; Today</p>
+         </div>
+       </figcaption>
+      </figure>
+    <div class="testimonials__teastimonial">
+     <p>The best online zoo I’ve met. My son delighted very much
+         ljves to watch gouillas. Online zoo is one jf the ways to
+         instill a love for animals.The best online zoo I’ve met. My
+         son delighted very much ljves to watch gouillas. Online zoo
+         is one jf the ways to instill a love for animals.</p>
+     <p class="lastp">The best online zoo I’ve met. My son delighted very much ljves to
+         watch
+         gouillas.
+         Online zoo is one jf the ways to instill a love for animals.The best online zoo
+         I’ve
+         met. My son delighted very much ljves to watch gouillas. Online zoo is one jf
+         the
+         ways to instill a love for </p>
+       </div>
+       </div>`
+
+    element.classList.toggle("active");
+    const mask=document.querySelector(".page-mask");
+    mask.classList.toggle("active");
+    mask.addEventListener("click",()=>{
+      mask.classList.remove("active");
+      element.classList.remove("active");
+    })
+    const xIcon=document.createElement("i");
+    xIcon.classList.add("fa-solid","fa-xmark","popup-xmark");
+    element.appendChild(xIcon);
+        xIcon.addEventListener("click",()=>{
+          element.classList.remove("active");
+          mask.classList.remove("active");
+        })
+      });
+   }
